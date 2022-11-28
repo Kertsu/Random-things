75b90f49b6f482e2434b4af5e281039cf50f75a8
Kertsu <114039848+Kertsu@users.noreply.github.com>
2022-11-28 23:58:47 +0800

    Simplified

--- tiny.py
+++ tiny.py
@@ -1,18 +1,11 @@
 def tiny(list_1, list_2, limit):
-    global bools
-    storage = []
-    bools = []
     length = len(list_1)
-    
+    answer = []
     for i in range(length):
-        storage.append(str(list_1[i]) + str(list_2[(length-1)-i]))
-    
-    for i in range(length):
-        if limit < int(storage[i]):
-            bools.append(False)
-        else:
-            bools.append(True)
-    return bools
-    
-if tiny([1,2,3,4,5], [6,7,8,9,10], 100) != None:
-    print(bools)
+         concatenations = str(list_1[i]) + str(list_2[(length-1)-i])
+         
+         if limit < int(concatenations):
+             answer.append(False)
+         else:
+             answer.append(True)
+    return answer
